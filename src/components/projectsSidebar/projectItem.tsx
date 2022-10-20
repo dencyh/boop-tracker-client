a896898787c0b5df7ed2e412a7d604d3344fbca7
Alexander Moryan <a.dencyh@gmail.com>
2022-10-20 22:50:57 +0300

    wip edit project

--- src/components/projectsSidebar/projectItem.tsx
+++ src/components/projectsSidebar/projectItem.tsx
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from "react";
 import { IProject } from "../../models/IProject";
-import { faCircleDot } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCircleDot,
+  faPenToSquare
+} from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { observer } from "mobx-react-lite";
 import { Context } from "../..";
-import { Link } from "react-router-dom";
 
 type ProjectItemProps = {
   project: IProject;
@@ -19,7 +21,13 @@ const ProjectItem = ({ project }: ProjectItemProps) => {
       onClick={() => {
         store.setCurrentProject(project);
       }}
+      className="relative"
     >
+      {project.id === store.currentProject.id && (
+        <button className="absolute right-6 top-1/2 h-8 w-8 -translate-y-1/2 rounded-lg ring-1 ring-inset ring-gray-500 hover:bg-gray-600 hover:text-white">
+          <FontAwesomeIcon icon={faPenToSquare} />
+        </button>
+      )}
       <button
         className={`my-1 mr-0 flex h-full w-full gap-4 rounded-l-xl border-r-0  p-3 text-left font-bold text-gray-600 hover:bg-white focus:ring-4 focus:ring-gray-200 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800  dark:focus:ring-gray-800
           ${project.id === store.currentProject.id ? "bg-white" : ""}
